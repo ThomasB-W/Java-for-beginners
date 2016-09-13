cba06047b905372ac0082af9d30a2a14e1f5a265
ThomasB-W <tberrisford@utcmediacityuk.org.uk>
2016-09-13 10:46:09 +0100

    Add files via upload

---
+++ AlwaysMakeNewProjects.java
@@ -0,0 +1,45 @@
+package alwaysmakenewprojects;
+
+public class AlwaysMakeNewProjects {
+
+    public static void main(String[] args) {
+        String colour="Purple";
+        String name="Thomas";
+        String street="Irlam";
+        System.out.println("Your favourite colour is: "+colour);
+        System.out.println("Your name is: "+name);
+        System.out.println("Your street is: "+street);
+        
+        int number = 5;
+        int age = 16;
+        int shoe = 12;
+        System.out.println("Your chosen number was: "+number);
+        System.out.println("Your age is: "+age);
+        System.out.println("Your shoe size is: "+shoe);
+        
+        double test = 1.5;
+        double pi = 3.14;
+        System.out.println(""+test);
+        System.out.println(""+pi);
+        // Are of a circle = pi x r^2
+        // speed = distance/time
+        
+        System.out.println(age + shoe);
+        System.out.println(age - shoe);
+        System.out.println(age * shoe);
+        System.out.println(age / shoe);
+        // * = Multiplication 
+        // / = Divide    }
+        
+        int x = 10;
+        int y = 5;
+        
+        if (x < y) {
+           // If the condition is true
+            System.out.println("x is less than y");
+        }else{
+            // If the condition is false
+            System.out.println("x is more than y");
+        }
+}
+}
